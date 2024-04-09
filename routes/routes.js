6d21e009f85f1de2dc270aab75f6c2a1191560c3
Ian <barbosaianlucas@gmail.com>
2024-04-09 20:27:21 -0300

    Alteração das rotas

---
+++ routes/routes.js
@@ -0,0 +1,13 @@
+const { Router } = require("express")
+const express = require("express")
+const controller = require("../controllers/controller") 
+
+const routes = express.Router()
+
+routes.get("/", controller.getAll)
+routes.get("/:id",controller.getId)
+routes.post("/", controller.createNew)
+routes.put("/:id", controller.updateData)
+routes.delete("/;id", controller.deleteData)
+
+module.exports = routes
